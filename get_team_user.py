890cbc0ce53bb2dbdfdcd4fe97d6b11e97344e8d
Li Muzhi <zeus1014_2023@163.com>
2024-10-25 15:57:41 +0800

    null

--- get_team_user.py
+++ get_team_user.py
@@ -6,7 +6,7 @@ import pyperclip
 cookie = '__client_id=627f4c1bf139fa5ef97dab418b44481b3c60cdd1;_uid=787042;C3VK=3c2971'
 headers = {'User-Agent': 
            'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36 Edg/130.0.0.0',}
-print('洛谷团队成员读取机，@demons1014出品')
+print('洛谷团队群发机，demons1014出品')
 team_id = int(input('输入团队id: '))
 html = requests.get(f"https://www.luogu.com.cn/api/team/members/{team_id}", headers = headers).text
 
@@ -31,4 +31,4 @@ for i in range(0,len(at_list), step):
         print(user, end='')
     pyperclip.copy(tmp)
     input(f"已将{cnt}个用户名复制到剪贴板，发送后请按回车……")
-print('所以用户名已输出完毕，感谢您使用本软件')
+print('所有用户名已输出完毕，感谢您使用本软件')
